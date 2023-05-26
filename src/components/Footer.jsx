7f595cbe0d9482d7ddf7b83f7aa1152208d6d046
Andresito <andresfelipeacero99@gmail.com>
2023-05-26 18:58:13 -0500

    [CREATE] Carrito de Compras

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import './Footer.css'
+
+export function Footer () {
+
+
+  return (
+    <footer className='footer'>
+      <h4>Trabajo en equipo e-commerce:</h4>
+        <p><span>🎶Andres Muñoz</span></p>
+        <p><span>🎶Andrea Luque</span></p>
+        <p><span>🎶Nicolás Méndez</span></p>
+      <h5>Carrito de compras, Vinilos Maker</h5>
+    </footer>
+  )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import { Filters } from './Filters.jsx'
+
+export function Header () {
+  return (
+    <header>
+      <h1>VINILOS MAKER💽</h1>
+      <Filters />
+    </header>
+  )
+}
